48d4c98bfb361ae7c6e0c6cf68a5ae9a6b9b827f
sumer alkadasi <sumer5020@gmail.com>
2022-03-20 21:39:15 +0300

    Delete SinglePage directory

--- SinglePage/src/main.js
+++
@@ -1,7 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import VueSmoothScroll from 'vue3-smooth-scroll'
-
-const app = createApp(App)
-app.use(VueSmoothScroll)
-app.mount('#app')
